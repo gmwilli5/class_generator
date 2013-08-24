49553a653b2c0146c604df0d39c765263643c42e
gmwilli5 <gmwilli5@ncsu.edu>
2013-08-24 00:27:55 -0400

    added to todo list

--- todo.cpp
+++ todo.cpp
@@ -1,2 +1,4 @@
-///1: generate .cpp file
-///2: add variables with std::vector
+//1::done generate .cpp file
+///2: add variables with std::vector (have input include type)
+///3: add funcyions with std::vector (have input type the whole function prototype without semicolon)
+///4: conider support for template classes
